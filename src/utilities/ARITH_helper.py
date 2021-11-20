542721e13efe39d06c40a7b9a36133d568441ca1
clumsyyyy <owencwijaya@gmail.com>
2021-11-20 21:00:16 +0700

    bugfix salah indent

--- src/utilities/ARITH_helper.py
+++ src/utilities/ARITH_helper.py
@@ -1,58 +1,56 @@
-import re
+from LOOP_FA_function import FA_VALIDFUNVARNAMEC, FA_function_HELPER
 
 class arithHelper:
-    def checkArithStatement(self, str):
+    def checkArithStatement(self, string):
         arithOps = ["+", "-", "/", "//", "%", "*"]
         arithFlag = False
-        if re.search('[a-zA-Z]', str):
-            raise Exception("there is alphabet")
         for i in range(len(arithOps)):
-            if arithOps[i] in str:
+            if arithOps[i] in string:
                 arithFlag = True
         for i in range(len(arithOps)):
-            if (str[0] == arithOps[i] or str[-1] == arithOps[i]) and arithOps[i] != "-":
+            if (string[0] == arithOps[i] or string[-1] == arithOps[i]) and arithOps[i] != "-":
                 raise Exception("misplaced operator")
 
         if arithFlag == False:
             raise Exception("No operator in sentence")
         
-        openingBracketCount = str.count("(")
-        closingBracketCount = str.count(")")
+        openingBracketCount = string.count("(")
+        closingBracketCount = string.count(")")
         if (openingBracketCount != closingBracketCount):
             raise Exception("Non-matching brackets")
         arithCount = 0
         for i in range(len(arithOps)):
-            if arithOps[i] in str:
+            if arithOps[i] in string:
                 if (arithOps[i] == "-"):
-                    arr = [x for x in str]
+                    arr = [x for x in string]
                     for j in range(len(arr)):
                         if arr[j] == "-" and not arr[j + 1].isdigit():
                             arithCount += 1
                 elif (arithOps[i] == "/"):
-                    arr = [x for x in str]
+                    arr = [x for x in string]
                     for j in range(len(arr)):
                         if arr[j] == "/" and (arr[j + 1] != "/" and arr[j - 1] != "/"):
                             arithCount += 1
                 else:
-                    arithCount += str.count(arithOps[i])
+                    arithCount += string.count(arithOps[i])
 
-        if "(" in str:
-            str = str.replace("(", "")
-        if ")" in str:
-            str = str.replace(")", "")
-        
-        if (arithCount >= len(self.arithTokenizer(str))):
+        if "(" in string:
+            string = string.replace("(", "")
+        if ")" in string:
+            string = string.replace(")", "")
+        if (arithCount >= len(self.arithTokenizer(string))):
             raise Exception("Too many arithmetic operators")
         else:
             return True
     
-    def arithTokenizer(self, str):
+    def arithTokenizer(self, string):
         num_arr = []
-        arr = [x for x in str]
+        arr = [x for x in string]
+        arithOps = ["+", "-", "/", "//", "%", "*"]
         count = ""
         minusFlag = False
         for i in range(len(arr)):
-            if arr[i].isdigit():
+            if arr[i].isdigit() or arr[i].isalpha():
                 count += arr[i]
             elif arr[i] == "-":
                 if arr[i + 1].isdigit() and arr[i - 1] != "-":
@@ -65,21 +63,35 @@ class arithHelper:
                         else:
                             num_arr.append(int(count))
                         count = ""
-            elif arr[i].isalpha():
-                num_arr.append(arr[i])
             else:
                 if count != "":
-                    if minusFlag:
-                        minusFlag = False
-                        num_arr.append(int(count) * -1)
+                    if count.isdigit():
+                        if minusFlag:
+                            minusFlag = False
+                            num_arr.append(int(count) * -1)
+                        else:
+                            num_arr.append(int(count))
+                        count = ""
                     else:
-                        num_arr.append(int(count))
-                    count = ""
-            if count != "":
-                    if minusFlag:
-                        minusFlag = False
-                        num_arr.append(int(count) * -1)
-                    else:
-                        num_arr.append(int(count))
-                    count = ""
+                        num_arr.append(count)
+        if count != "":
+            if count.isdigit():
+                if minusFlag:
+                    minusFlag = False
+                    num_arr.append(int(count) * -1)
+                else:
+                    num_arr.append(int(count))
+                count = ""
+            else:
+                num_arr.append(count)
+        print(num_arr)
+
+        varCheck = FA_VALIDFUNVARNAMEC()
+        for i in range(len(num_arr)):
+            if not str(num_arr[i]).lstrip("-").isdigit():
+                try:
+                    varCheck.check(num_arr[i])
+                except Exception as e:
+                    raise e
+   
         return num_arr
